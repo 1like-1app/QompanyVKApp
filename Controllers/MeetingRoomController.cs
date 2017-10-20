e91b47096dbcad6baa7f80555c114f124b6b0968
Yury Belousov <yury-belousov@yandex.ru>
2017-10-20 18:51:32 +0300

    Add sample model

---
+++ Controllers/MeetingRoomController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QompanyVKApp.Models;
+
+namespace QompanyVKApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class MeetingRoomController : Controller
+    {
+        [HttpGet("[action]")]
+        public IEnumerable<MeetingRoom> GetRooms()
+        {
+            return null;
+        }
+    }
+}

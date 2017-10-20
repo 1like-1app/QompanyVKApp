ceb1f41815ed70010ecea5d84ebec15de05406ca
Sergey <piteryo7@gmail.com>
2017-10-20 21:02:07 +0400

    Azure integration implementation (#2)
    * Add sample model
    
    * Add database to azure

---
+++ Models/MeetingRoom.cs
@@ -0,0 +1,8 @@
+﻿namespace QompanyVKApp.Models
+{
+    public class MeetingRoom 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ QompanyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QompanyVKApp.Models;
+
+namespace QompanyVKApp
+{
+    public class QompanyDbContext : DbContext
+    {
+        public QompanyDbContext(DbContextOptions<QompanyDbContext> options) : base(options)
+        {}
+
+        public DbSet<MeetingRoom> MeetingRooms { get; set; }
+    }
+}

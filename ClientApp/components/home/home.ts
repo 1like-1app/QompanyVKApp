ca4b30168baa753ebb12c33a46ac4a6046857e6e
Yury Belousov <yury-belousov@yandex.ru>
2017-10-21 20:43:06 +0300

    adding participants to meeting (#12)
    * /final_menu:
    
    top design done!
    
    * top design fixed
    
    * col-xs fixed
    
    * Add modal of new members
    
    * change list to v-for
    
    * First attempts to implement setting speakers
    
    * fix showing chosen employees
    
    * scroll for participants

--- ClientApp/components/home/home.ts
+++ ClientApp/components/home/home.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import { Meeting } from "../../models/meeting"
 import { Component } from 'vue-property-decorator';
+import { Employee } from '../../models/Employee';
 
 
 @Component
@@ -8,6 +9,19 @@ export default class BookingForm extends Vue {
     date: string = new Date().toISOString().split('T')[0];
     meeting: Meeting = new Meeting();
     meetings: Meeting[] = [];
+    employees: Employee[] = [];
+    checkboxToggle(id: number) {
+        let b = this.meeting.employeeMeetings.filter(e => e.id == id);
+        if (b.length) {
+            this.meeting.employeeMeetings = this.meeting.employeeMeetings.filter(e => e.id != b[0].id);
+        }
+        else {
+            this.meeting.employeeMeetings.push(this.employees.filter(e => e.id == id)[0]);
+        }
+        //alert(JSON.stringify(this.meeting.employeeMeetings));
+    }
+
+
     onSubmit(submitEvent: any) {
         this.meeting.startTime = new Date(this.date.toString() + 'T' + this.meeting.startTime.toString());
         this.meeting.endTime = new Date(this.date.toString() + 'T' + this.meeting.endTime.toString());
@@ -29,11 +43,15 @@ export default class BookingForm extends Vue {
             .then(data => {
                 this.meetings = data;
                 for (var i = 0; i < this.meetings.length; ++i) {
-                    this.meetings[i].date = new Date(this.meetings[i].startTime).toISOString().split('T')[0].replace(/-/g,"/");
+                    this.meetings[i].date = new Date(this.meetings[i].startTime).toISOString().split('T')[0].replace(/-/g, "/");
                     this.meetings[i].title = this.meetings[i].theme;
                 }
             });
-        
+        fetch('api/Employees/GetEmployees')
+            .then(response => response.json() as Promise<Employee[]>)
+            .then(data => {
+                this.employees = data;
+            });
     }
 
 }

4137b5a11dca0a75ebb4171ea4cb82c8e0a7375e
Sergey <piteryo7@gmail.com>
2017-10-21 22:45:14 +0300

    NameFix

--- Controllers/MeetingRoomsController.cs
+++ Controllers/MeetingRoomsController.cs
@@ -52,7 +52,7 @@ namespace QompanyVKApp.Controllers
         /// api/meetingRooms/GetSatisfyingRoom/2009-06-15T13:45:30/2012-04-09T11:59:32
         /// </example>
         [HttpGet("[action]/{startTime}/{endTime}")]
-        public IEnumerable<MeetingRoom> GetSatisfyingRoom([FromRoute]DateTime startTime, DateTime endTime)
+        public IEnumerable<MeetingRoom> GetSatisfyingRooms([FromRoute]DateTime startTime, DateTime endTime)
         {
             var meetingRooms = _context.MeetingRooms.Include(mr => mr.Meetings).ToList();
             var k =  meetingRooms.Where(mr => mr.Meetings.Any(x => x.EndTime < startTime || x.StartTime > endTime)).ToList();

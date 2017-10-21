bb11809177401140b5e5a75708250f9e4d512da9
Yury Belousov <yury-belousov@yandex.ru>
2017-10-21 03:49:23 +0300

    Add Theme to Meeting

--- Models/EmployeeMeeting.cs
+++ Models/EmployeeMeeting.cs
@@ -1,5 +1,14 @@
 ﻿namespace QompanyVKApp.Models
 {
+    /// <summary>
+    /// Many-to-many relationship between Employee and Meeting
+    /// Because 1 Employee can participate in many Meetings &
+    /// In 1 Meeting can participate many Employees
+    /// </summary>
+    /// <remarks>
+    /// We really want if it could be done autimatic, but EF Core doesn't support it
+    /// </remarks>
+    /// <seealso cref="https://github.com/aspnet/EntityFrameworkCore/issues/1368"/>
     public class EmployeeMeeting
     {
         public int EmployeeId { get; set; }

194874f5672487ded84fdf83d55b184cf5cc25d7
Yury Belousov <yury-belousov@yandex.ru>
2017-10-22 04:58:33 +0300

    add base group model

---
+++ Models/Group.cs
@@ -0,0 +1,10 @@
+﻿namespace QompanyVKApp.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        // ReSharper disable once InconsistentNaming
+        public string VKId { get; set; }
+        public string AccessToken { get; set; }
+    }
+}

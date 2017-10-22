0e2728bef723a1c37cc70dedba0b18228cf05d36
Yury Belousov <yury-belousov@yandex.ru>
2017-10-22 10:30:28 +0300

    little fix

--- Controllers/MeetingsController.cs
+++ Controllers/MeetingsController.cs
@@ -88,6 +88,7 @@ namespace QompanyVKApp.Controllers
                 return BadRequest(ModelState);
             }
             meeting.MeetingRoom = _context.MeetingRooms.Single(r => r.Id == meeting.MeetingRoom.Id);
+            meeting.EmployeeMeetings = null;
             _context.Meetings.Add(meeting);
             await _context.SaveChangesAsync();
 
